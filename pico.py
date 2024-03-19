faba3e195fb174e311ab6651ee91d05b298eb848
41-6C-61-6E <158097564+41-6C-61-6E@users.noreply.github.com>
2024-03-19 10:34:06 -0700

    Update pico.py

--- pico.py
+++ pico.py
@@ -4,15 +4,6 @@
 import machine
 import time
 
-
-###### OTA Updates
-from ota import OTAUpdater
-from WIFI_CONFIG import SSID, PASSWORD
-firmware_url = "https://raw.githubusercontent.com/41-6C-61-6E/pico"
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "pico.py")
-ota_updater.download_and_install_update_if_available()
-######
-
 led = machine.Pin('LED', machine.Pin.OUT) #configure LED Pin as an output pin and create and led object for Pin class
 
 while True:
